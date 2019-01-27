dd9bd477926450bea6f614731a77fa1a8d7fdbda
albertgoncalves <goncalves.albert@gmail.com>
2019-01-27 15:06:53 -0500

    Args, kwargs, margs.

---
+++ kwargs/kwargs.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+f = function(a, b, c) {
+    for (x in list("a", "b", "c")) {
+        y = eval(parse(text=x))
+        cat(paste(x, "=", y, "\n"))
+    }
+}
+
+if (sys.nframe() == 0) {
+    a = "String"
+    b = 1000L
+    c = TRUE
+
+    args = list(a, b, c)
+    kwargs = list(c=a, a=b, b=c)
+    mixed = list(c=a, c, b=b)
+
+    for (xs in list(args, kwargs, mixed)) {
+        cat("\n")
+        do.call(f, xs)
+    }
+}
